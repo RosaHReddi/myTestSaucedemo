1adcaccad28027da23654dc4e509726f801da4a3
Rosa <rosahreddi@gmail.com>
2024-12-11 16:55:34 -0300

    subiendo el nuevo cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,4 @@
+/*
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
@@ -8,4 +9,43 @@ module.exports = defineConfig({
     },
   },
 });
+*/
+
+
+//cambio la configuración para Correr specs (npx cypress run) & Uso de mocha para generar reportes
+/*
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+   chromeWebSecurity:false,
+   e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  video: true, // Guardar los videos
+  screenshotOnRunFailure: true, // Guardar las capturas de pantallas de los tests que fallaron
+  videosFolder: "cypress/videos", // Le decimos en que carpeta guardar los videos
+  screenshotsFolder: "cypress/screenshots", // Le decimos en que carpeta guardar las capturas
+  
+});
+*/
+
+//cambio la configuración para correr mocha 
+
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  chromeWebSecurity:false,
+  reporter: 'cypress-mochawesome-reporter', // Definimos el reporte a utilizar 
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on); // Configura los eventos de Node para que el plugin funcione
+    },
+  },
+});
+
+
+
+
 
